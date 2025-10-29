bfb1b8d765d645dcc8bc59ee54d1bfe2c53246b2
allymf <alyssonfacanhamoreira@gmail.com>
2025-10-29 13:20:16 -0300

    Initial Commit

---
+++ LimeLite/LimeLiteApp.swift
@@ -0,0 +1,17 @@
+//
+//  LimeLiteApp.swift
+//  LimeLite
+//
+//  Created by Alysson Moreira on 29/10/25.
+//
+
+import SwiftUI
+
+@main
+struct LimeLiteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
